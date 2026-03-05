dc45cc445afe96a48e7e3805e1ee75b14f4aae3c
Hamadellah <hamadellahotman13@gmail.com>
2026-03-05 11:55:17 +0000

    Add Header component and update TournmentCard layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { tournamentData } from "./data/tournmentDB";
 import TournmentCard from "./components/TournamenteCard";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
+      <Header/>
       <TournmentCard tournamentData={tournamentData} />
     </div>
   );
--- src/components/TournamenteCard.jsx
+++ src/components/TournamenteCard.jsx
@@ -1,24 +1,7 @@
 const TournmentCard = ({ tournamentData }) => {
   return (
     <div className="min-h-screen bg-slate-100 p-10">
-<div className="mt-[5%] bg-purple-600 text-white p-4 rounded-lg shadow-md">
-   <div className="flex items-center justify-between">
-     <h1 className="text-lg font-semibold"> Good Morning, <span className="font-bold">Samuel Walker!</span> </h1>
-      <div className="flex items-center gap-4">
-         
-         <button className="relative"> 
-          <span className="material-icons">notifications</span> 
-          </button> {/* Profile Picture */}
-           <img src="https://via.placeholder.com/40" alt="Profile" className="w-10 h-10 rounded-full border-2 border-white" /> </div> 
-           </div> {/* Search Bar */} 
-           <div className="mt-4 flex items-center bg-white rounded-lg px-3 py-2">
-             <span className="material-icons text-gray-500">search</span>
-              <input type="text" placeholder="Search..." className="flex-1 px-2 outline-none text-gray-700" />
-               <span className="material-icons text-gray-500">tune</span> 
-</div> 
-</div>
-
-      <div className="mt-[4%] grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="mt-[3%] grid md:grid-cols-2 lg:grid-cols-3 gap-6">
         {tournamentData.map((t) => (
           <div
             key={t.id}
