a839a2a053d1259e2372a00673ea328f1ff5c348
Hamadellah <hamadellahotman13@gmail.com>
2026-03-06 11:17:36 +0000

    Add Homepage component to display header and tournament cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 import { tournamentData } from "./data/tournmentDB";
 import Header from "./components/Header";
 import TournmentCard from "./components/TournamenteCard";
 import TournamentDetails from "./components/TournamentDetails";
+import Homepage from "./pages/Homepages";
 
 function App() {
   const [selectedTournament, setSelectedTournament] = useState(null);
   return (
-  <BrowserRouter>
-      <Header />
-
+    <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={<TournmentCard tournamentData={tournamentData} />}
-        />
+        <Route path="/" element={<Homepage />} />
 
-                <Route
+        <Route
           path="/tournament/:id"
           element={<TournamentDetails tournamentData={tournamentData} />}
         />
---
+++ src/pages/Homepages.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+import TournmentCard from "../components/TournamenteCard";
+import { tournamentData } from "../data/tournmentDB";
+function Homepage() {
+  return (
+    <>
+      <Header />
+      <TournmentCard tournamentData={tournamentData} />
+    </>
+  );
+}
+export default Homepage;
